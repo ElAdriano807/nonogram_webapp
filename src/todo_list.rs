e435448b0b26343ad7f8bedaf90e77d7eac29a39
Adrian Becker <adbecker@uni-bremen.de>
2024-02-23 19:27:41 +0100

    Fixed delete/edit bug and minimal css

--- src/todo_list.rs
+++ src/todo_list.rs
@@ -1,8 +1,8 @@
 use log::info;
-use web_sys::{HtmlInputElement, InputEvent, KeyboardEvent, MouseEvent};
+use stylist::css;
+use web_sys::{FocusEvent, HtmlInputElement, InputEvent, KeyboardEvent, MouseEvent};
 use yew::{Callback, Component, Context, function_component, Html, html, Properties, TargetCast};
-use Message::{Add, Search, DeleteAll, ToggleComplete};
-use crate::todo_list::Message::{Delete, Edit};
+use TodoListMessage::{Add, Search, DeleteAll, ToggleComplete, Delete, Edit};
 
 #[derive(Clone, PartialEq)]
 struct Todo {
@@ -10,7 +10,7 @@ struct Todo {
     completed: bool,
 }
 
-pub enum Message {
+pub enum TodoListMessage {
     Add(String),
     Search(String),
     ToggleComplete(usize),
@@ -18,13 +18,12 @@ pub enum Message {
     Delete(usize),
     DeleteAll,
 }
-
 pub struct TodoList {
     todos: Vec<Todo>,
     filter: String,
 }
 impl Component for TodoList {
-    type Message = Message;
+    type Message = TodoListMessage;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
@@ -149,10 +148,7 @@ impl TodoList {
 }
 
 enum TodoListItemMessage {
-    Edit
-}
-struct TodoListItem {
-    edit_mode: bool,
+    Edit(bool)
 }
 #[derive(Properties, PartialEq)]
 struct TodoListItemProps {
@@ -160,6 +156,9 @@ struct TodoListItemProps {
     toggle_complete_onclick: Callback<MouseEvent>,
     delete_onclick: Callback<MouseEvent>,
     edit_onkeypress: Callback<KeyboardEvent>,
+}
+struct TodoListItem {
+    edit_mode: bool,
 }
 impl Component for TodoListItem {
     type Message = TodoListItemMessage;
@@ -172,34 +171,20 @@ impl Component for TodoListItem {
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            TodoListItemMessage::Edit => {
+            TodoListItemMessage::Edit(edit) => {
                 info!("Receive Edit");
-                self.edit_mode ^= true;
+                self.edit_mode = edit;
                 true
             }
         }
     }
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let edit_onclick: Callback<MouseEvent> = ctx.link().callback(|_| {
-            info!("Send Edit");
-            Self::Message::Edit
-        });
-        let callback = ctx.props().edit_onkeypress.clone();
-        let edit_onkeypress: Callback<KeyboardEvent> = ctx.link().batch_callback(move |e: KeyboardEvent| {
-            callback.emit(e.clone());
-            if e.key() == "Enter" {
-                info!("Send Edit");
-                Some(Self::Message::Edit)
-            } else {
-                None
-            }
-        });
         html! {
             <li>
                 <input type="checkbox" checked={ctx.props().todo.completed} onclick={ctx.props().toggle_complete_onclick.clone()}/>
                 { if self.edit_mode {
                       html!{
-                          <input value={ctx.props().todo.description.clone()} onkeypress={edit_onkeypress} placeholder="What needs to be done?"/>
+                          <input value={ctx.props().todo.description.clone()} onkeypress={Self::edit_onkeypress(ctx)} placeholder="What needs to be done?"/>
                       }
                   } else {
                       html!{
@@ -207,60 +192,85 @@ impl Component for TodoListItem {
                       }
                   }
                 }
-                <button onclick={ctx.props().delete_onclick.clone()}>{"Delete"}</button>
-                <button onclick={edit_onclick}>{if self.edit_mode {"Cancel"} else {"Edit"}}</button>
+                <button onclick={Self::delete_onkeypress(ctx)}>{"Delete"}</button>
+                <button onclick={Self::edit_onclick(ctx, self.edit_mode)}>{if self.edit_mode {"Cancel"} else {"Edit"}}</button>
             </li>
         }
     }
 }
 
-struct TodoInput;
+impl TodoListItem {
+    fn edit_onclick(ctx: &Context<Self>, edit_mode: bool) -> Callback<MouseEvent> {
+        ctx.link().callback(move |_| {
+            info!("Send Edit");
+            TodoListItemMessage::Edit(!edit_mode)
+        })
+    }
+    fn edit_onkeypress(ctx: &Context<Self>) -> Callback<KeyboardEvent> {
+        let callback = ctx.props().edit_onkeypress.clone();
+        ctx.link().batch_callback(move |e: KeyboardEvent| {
+            callback.emit(e.clone());
+            if e.key() == "Enter" {
+                info!("Send Edit");
+                Some(TodoListItemMessage::Edit(false))
+            } else {
+                None
+            }
+        })
+    }
+    fn delete_onkeypress(ctx: &Context<Self>) -> Callback<MouseEvent> {
+        let callback = ctx.props().delete_onclick.clone();
+        ctx.link().callback(move |e: MouseEvent| {
+            callback.emit(e.clone());
+            info!("Send Edit");
+            TodoListItemMessage::Edit(false)
+        })
+    }
+}
+
 #[derive(Properties, PartialEq)]
 struct TodoInputProps {
     add_onkeypress: Callback<KeyboardEvent>,
 }
-impl Component for TodoInput {
-    type Message = ();
-    type Properties = TodoInputProps;
-    fn create(_ctx: &Context<Self>) -> Self { Self }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html!{
-            <input onkeypress={ctx.props().add_onkeypress.clone()} placeholder="What needs to be done?"/>
-        }
+#[function_component]
+fn TodoInput(props: &TodoInputProps) -> Html {
+    html!{
+        <input onkeypress={props.add_onkeypress.clone()} placeholder="What needs to be done?"/>
     }
 }
 
-struct DeleteAllTodosButton;
 #[derive(Properties, PartialEq)]
 struct DeleteAllTodosButtonProps {
     delete_all_onclick: Callback<MouseEvent>
 }
-impl Component for DeleteAllTodosButton {
-    type Message = ();
-    type Properties = DeleteAllTodosButtonProps;
-    fn create(_ctx: &Context<Self>) -> Self { Self }
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <button onclick={ctx.props().delete_all_onclick.clone()}>
-                {"Delete All Todos"}
-            </button>
-        }
+#[function_component]
+fn DeleteAllTodosButton(props: &DeleteAllTodosButtonProps) -> Html {
+    html! {
+        <button onclick={props.delete_all_onclick.clone()}>
+            {"Delete All Todos"}
+        </button>
     }
 }
 
-struct FilterInput;
 #[derive(Properties, PartialEq)]
 struct FilterInputProps {
     filter_oninput: Callback<InputEvent>,
 }
-impl Component for FilterInput {
-    type Message = ();
-    type Properties = FilterInputProps;
-    fn create(_ctx: &Context<Self>) -> Self { Self }
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <input type="text" oninput={ctx.props().filter_oninput.clone()} placeholder="Filter"/>
-        }
+#[function_component]
+fn FilterInput(props: &FilterInputProps) -> Html {
+    let style = css!(r#"
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        box-sizing: border-box;
+        border: 2px solid red;
+        border-radius: 4px;
+    "#);
+    let onfocus = Callback::from(|e: FocusEvent| {
+
+    });
+
+    html! {
+        <input class={style} type="text" oninput={props.filter_oninput.clone()} placeholder="Filter"/>
     }
 }
